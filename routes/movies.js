194505811329814d8f15e53c4c19bc808f1286c2
deepak-sdk <itisdeepakkumar@outlook.com>
2021-12-01 04:42:57 +0530

    router chaining

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { getMovies, createMovies, getMovieByID, deleteMovieByID, updateMovieByID } from '../helper.js';
+import {
+  getMovies,
+  createMovies,
+  getMovieByID,
+  deleteMovieByID,
+  updateMovieByID,
+} from "../helper.js";
 
 // app.get("/movies", (req, res) => {
 //   res.send(movies);
@@ -29,80 +35,83 @@ import { getMovies, createMovies, getMovieByID, deleteMovieByID, updateMovieByID
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  console.log(req.query);
-  const filter = req.query;
-  console.log(filter);
-  if (filter.rating) {
-    filter.rating = +filter.rating;
-  }
-  // db.movies.find({language:"English"})
+router
+  .route("/")
+  .get(async (req, res) => {
+    console.log(req.query);
+    const filter = req.query;
+    console.log(filter);
+    if (filter.rating) {
+      filter.rating = +filter.rating;
+    }
+    // db.movies.find({language:"English"})
 
-  // console.log(language, rating);
-  // let filteredMovies = movies;
+    // console.log(language, rating);
+    // let filteredMovies = movies;
 
-  // if (language) {
-  //   filteredMovies = filteredMovies.filter((mv) => mv.language === language);
-  // }
+    // if (language) {
+    //   filteredMovies = filteredMovies.filter((mv) => mv.language === language);
+    // }
 
-  // if (rating) {
-  //   filteredMovies = filteredMovies.filter((mv) => mv.rating === +rating);
-  // }
+    // if (rating) {
+    //   filteredMovies = filteredMovies.filter((mv) => mv.rating === +rating);
+    // }
 
-  // db.movies.find({})
+    // db.movies.find({})
 
-  const filteredMovies = await getMovies(filter);
+    const filteredMovies = await getMovies(filter);
 
-  console.log(filteredMovies);
+    console.log(filteredMovies);
 
-  filteredMovies && res.send(filteredMovies);
-});
+    filteredMovies && res.send(filteredMovies);
+  })
+  .post(async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    //  Create movies in mongo - db.movies.insertMany(data)
+    const result = await createMovies(data);
+    res.send(result);
+  });
 
 // Create
-router.post("/", async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  //  Create movies in mongo - db.movies.insertMany(data)
-  const result = await createMovies(data);
-  res.send(result);
-});
+router
+  .route("/:id")
+  // capturing id using find
+  .get(async (req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
 
-// capturing id using find
-router.get("/:id", async (req, res) => {
-  console.log(req.params);
-  const { id } = req.params;
+    // db.movies.findOne({key:value})
+    const movie = await getMovieByID(id);
+    // const movie = movies.find((mv) => mv.id === id);
+    console.log(movie);
+    movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
+  })
 
-  // db.movies.findOne({key:value})
-  const movie = await getMovieByID(id);
-  // const movie = movies.find((mv) => mv.id === id);
-  console.log(movie);
-  movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
-});
+  // delete
+  .delete(async (req, res) => {
+    const { id } = req.params;
 
-// delete
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
+    // db.movies.deleteOne({key:value})
+    const remainingMovies = await deleteMovieByID(id);
 
-  // db.movies.deleteOne({key:value})
-  const remainingMovies = await deleteMovieByID(id);
+    console.log(remainingMovies);
+    remainingMovies.deletedCount > 0
+      ? res.send(remainingMovies)
+      : res.status(404).send({ message: "NOT FOUND" });
+  })
 
-  console.log(remainingMovies);
-  remainingMovies.deletedCount > 0
-    ? res.send(remainingMovies)
-    : res.status(404).send({ message: "NOT FOUND" });
-});
+  // Edit
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
 
-// Edit
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
+    // db.movies.updateOne({key:value},, { $set: data })
+    const updatedMovie = await updateMovieByID(id, data);
+    const movie = await getMovieByID(id);
 
-  // db.movies.updateOne({key:value},, { $set: data })
-  const updatedMovie = await updateMovieByID(id, data);
-  const movie = await getMovieByID(id);
-
-  console.log(movie);
-  movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
-});
+    console.log(movie);
+    movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
+  });
 
 export const moviesRouter = router;
