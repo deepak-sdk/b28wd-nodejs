69938a3c9e4ec1878ee65628a137736a064595f7
deepak-sdk <itisdeepakkumar@outlook.com>
2021-12-01 04:18:03 +0530

    routes-movies

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import express from "express";
 import { MongoClient } from "mongodb";
 import { getMovies, createMovies, getMovieByID, deleteMovieByID, updateMovieByID } from './helper.js';
+import { moviesRouter } from './routes/movies.js';
 
 
 dotenv.config() // all keys stored in process.env
@@ -33,111 +34,7 @@ app.get("/", (req, res) => {
   res.send("Hello World😀😄");
 });
 
-// app.get("/movies", (req, res) => {
-//   res.send(movies);
-// });
-
-// capturing id using filter
-// app.get("/movies/:id", (req, res) => {
-//   console.log(req.params);
-//   const { id } = req.params;
-//   const movie = movies.filter((mv) => mv.id === id)[0];
-//   res.send(movie);
-// });
-
-// app.get("/movies", (req, res) => {
-//   // request => query params
-//   console.log(req.query);
-//   const { year } = req.query;
-//   console.log(year);
-
-//   if (year) {
-//     const movie = movies.filter((mv) => mv.year === year);
-//     res.send(movie);
-//   } else {
-//     res.send(movies);
-//   }
-// });
-
-app.get("/movies", async (req, res) => {
-  console.log(req.query);
-  const filter = req.query;
-  console.log(filter);
-  if (filter.rating) {
-    filter.rating = +filter.rating;
-  }
-  // db.movies.find({language:"English"})
-
-  // console.log(language, rating);
-  // let filteredMovies = movies;
-
-  // if (language) {
-  //   filteredMovies = filteredMovies.filter((mv) => mv.language === language);
-  // }
-
-  // if (rating) {
-  //   filteredMovies = filteredMovies.filter((mv) => mv.rating === +rating);
-  // }
-
-  // db.movies.find({})
-
-  const filteredMovies = await getMovies(filter)
-
-  console.log(filteredMovies);
-
-  filteredMovies && res.send(filteredMovies);
-});
-
-// Create
-app.post("/movies", async (req, res) => {
-
-
-  const data = req.body;
-  console.log(data);
-  //  Create movies in mongo - db.movies.insertMany(data)
-  const result = await createMovies(data)
-  res.send(result);
-
-});
-
-// capturing id using find
-app.get("/movies/:id", async (req, res)=> {
-  console.log(req.params);
-  const { id } = req.params;
-
-  // db.movies.findOne({key:value})
-  const movie = await getMovieByID(id)
-  // const movie = movies.find((mv) => mv.id === id);
-  console.log(movie);
-  movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
-});
-
-// delete
-app.delete("/movies/:id", async (req, res)=> {
-  const { id } = req.params;
-
-  // db.movies.deleteOne({key:value})
-  const remainingMovies = await deleteMovieByID(id)
-
-  console.log(remainingMovies);
-  remainingMovies.deletedCount > 0
-    ? res.send(remainingMovies)
-    : res.status(404).send({ message: "NOT FOUND" });
-});
-
-// Edit
-app.put("/movies/:id", async (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
-
-  // db.movies.updateOne({key:value},, { $set: data })
-  const updatedMovie = await updateMovieByID(id, data)
-  const movie = await getMovieByID(id)
-
-  console.log(movie);
-  movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
-
-});
+app.use("/movies", moviesRouter)
 
 app.listen(PORT, () => {
   console.log("App Started", PORT);
---
+++ routes/movies.js
@@ -0,0 +1,107 @@
+import express from "express";
+
+// app.get("/movies", (req, res) => {
+//   res.send(movies);
+// });
+
+// capturing id using filter
+// app.get("/movies/:id", (req, res) => {
+//   console.log(req.params);
+//   const { id } = req.params;
+//   const movie = movies.filter((mv) => mv.id === id)[0];
+//   res.send(movie);
+// });
+
+// app.get("/movies", (req, res) => {
+//   // request => query params
+//   console.log(req.query);
+//   const { year } = req.query;
+//   console.log(year);
+
+//   if (year) {
+//     const movie = movies.filter((mv) => mv.year === year);
+//     res.send(movie);
+//   } else {
+//     res.send(movies);
+//   }
+// });
+
+const router = express.Router();
+
+router.get("/movies", async (req, res) => {
+  console.log(req.query);
+  const filter = req.query;
+  console.log(filter);
+  if (filter.rating) {
+    filter.rating = +filter.rating;
+  }
+  // db.movies.find({language:"English"})
+
+  // console.log(language, rating);
+  // let filteredMovies = movies;
+
+  // if (language) {
+  //   filteredMovies = filteredMovies.filter((mv) => mv.language === language);
+  // }
+
+  // if (rating) {
+  //   filteredMovies = filteredMovies.filter((mv) => mv.rating === +rating);
+  // }
+
+  // db.movies.find({})
+
+  const filteredMovies = await getMovies(filter);
+
+  console.log(filteredMovies);
+
+  filteredMovies && res.send(filteredMovies);
+});
+
+// Create
+router.post("/movies", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  //  Create movies in mongo - db.movies.insertMany(data)
+  const result = await createMovies(data);
+  res.send(result);
+});
+
+// capturing id using find
+router.get("/movies/:id", async (req, res) => {
+  console.log(req.params);
+  const { id } = req.params;
+
+  // db.movies.findOne({key:value})
+  const movie = await getMovieByID(id);
+  // const movie = movies.find((mv) => mv.id === id);
+  console.log(movie);
+  movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
+});
+
+// delete
+router.delete("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+
+  // db.movies.deleteOne({key:value})
+  const remainingMovies = await deleteMovieByID(id);
+
+  console.log(remainingMovies);
+  remainingMovies.deletedCount > 0
+    ? res.send(remainingMovies)
+    : res.status(404).send({ message: "NOT FOUND" });
+});
+
+// Edit
+router.put("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  // db.movies.updateOne({key:value},, { $set: data })
+  const updatedMovie = await updateMovieByID(id, data);
+  const movie = await getMovieByID(id);
+
+  console.log(movie);
+  movie ? res.send(movie) : res.status(404).send({ message: "NOT FOUND" });
+});
+
+export const moviesRouter = router;
